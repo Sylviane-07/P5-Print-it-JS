9247672eda8c3c8beecca930e11e86841a4bc6a7
Sylviane Farooq <sylvianepinto@hotmail.fr>
2022-12-30 00:09:06 +0100

    semicolon add to JS code

--- assets/script.js
+++ assets/script.js
@@ -18,33 +18,33 @@ const slides = [
 ]
 
 // get documents link to variables
-const banner = document.getElementById("banner")
-const arrowLeft = document.getElementById("left_arrow")
-const arrowRight = document.getElementById("right_arrow")
-const mainImg = document.querySelector("#banner > img")
-const mainImgText = document.querySelector("#banner > p")
+const banner = document.getElementById("banner");
+const arrowLeft = document.getElementById("left_arrow");
+const arrowRight = document.getElementById("right_arrow");
+const mainImg = document.querySelector("#banner > img");
+const mainImgText = document.querySelector("#banner > p");
 
 //slides length => total images counts
-const totalImg = slides.length
+const totalImg = slides.length;
 
 //left & right arrows eventListener to call function onclick
 arrowLeft.addEventListener ("click", function() {
-console.log("left")
-})
+console.log("left");
+});
 
 arrowRight.addEventListener ("click", function() {
-console.log("right")
-})
+console.log("right");
+});
 
 //Bullet points added by img in slides
 //Active bullet added to frist-child
 function bulletPoints() {
-	const dot = document.createElement("span")
-	dot.classList.add("dot")
-	const dots = document.querySelector("#banner .dots")
-	dots.appendChild(dot)
-	const dotSelected = document.querySelector(".dots :first-child")
-	dotSelected.classList.add("dot_selected")
+	const dot = document.createElement("span");
+	dot.classList.add("dot");
+	const dots = document.querySelector("#banner .dots");
+	dots.appendChild(dot);
+	const dotSelected = document.querySelector(".dots :first-child");
+	dotSelected.classList.add("dot_selected");
 }
 
-slides.forEach(bulletPoints)
+slides.forEach(bulletPoints);
